01cb1047bd90eb719fe306abf624af7ee7810c24
Stephen Downie <stephen@spicule.co.uk>
2017-07-18 15:38:05 +0100

    new stuff

--- reactive/openldap.py
+++ reactive/openldap.py
@@ -1,17 +1,28 @@
-from charms.reactive import when, when_not, set_state
-
+from charms.reactive import when, when_not, set_state, remove_state
+from charms.layer import snap
+from charmhelpers.core import hookenv
+from subprocess import check_output
 
 @when_not('openldap.installed')
 def install_openldap():
-    # Do your setup here.
-    #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
-    #
+    channel = hookenv.config('channel')
+    hookenv.status_set('maintenance', 'Installing openldap snap')
+    snap.install('openldap', channel=channel, devmode=True)
+    set_state('openldap.snaps.installed')
+    remove_state('openldap.components.started')
     set_state('openldap.installed')
+
+@when('openldap.installed')
+@when_not('openldap.configured')
+def configure_openldap():
+    hookenv.status_set('maintenance', 'Configuring openldap snap')
+    check_output(['openldap.slapd-config'])
+    hookenv.status_set('maintenance', 'openldap configured')
+    set_state('openldap.configured')
+
+@when('openldap.configured')
+def start_openldap():
+    hookenv.status_set('maintenance', 'Starting openldap snap')
+    check_output(['openldap.slapd'])
+    hookenv.status_set('active', 'openldap running')
+    set_state('openldap.running')
